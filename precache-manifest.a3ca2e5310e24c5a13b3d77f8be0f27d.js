b34ff067e18d33aac0b4f4d7b3466b9381139c0e
MuhammedMushfiqV <mushfiqmuhd707@gmail.com>
2020-07-29 20:22:21 +0530

    Updates

---
+++ precache-manifest.a3ca2e5310e24c5a13b3d77f8be0f27d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b613355514a3de53543ac23c274f7c58",
+    "url": "/Tinder-clone-pwa/index.html"
+  },
+  {
+    "revision": "8cd8c68c6f665ab88fab",
+    "url": "/Tinder-clone-pwa/static/css/main.647ea09b.chunk.css"
+  },
+  {
+    "revision": "b15ad4c5a9e7858b0bdc",
+    "url": "/Tinder-clone-pwa/static/js/2.a6d385a4.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Tinder-clone-pwa/static/js/2.a6d385a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cd8c68c6f665ab88fab",
+    "url": "/Tinder-clone-pwa/static/js/main.1f4c028a.chunk.js"
+  },
+  {
+    "revision": "58f62e58df089e77e284",
+    "url": "/Tinder-clone-pwa/static/js/runtime-main.875dea11.js"
+  }
+]);
